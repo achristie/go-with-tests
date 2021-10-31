bdf13787abbb04000b5a6a1d1aeeed5f0aeea844
Andrew Christie <awchristie86@gmail.com>
2021-10-31 19:17:50 -0400

    refactored hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "hello, world"
+	got := Hello("Andrew")
+	want := "Hello, Andrew"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
