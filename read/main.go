5f4cf49ee8a0fa886c816a167f915896c85ed953
Andrew Christie <awchristie86@gmail.com>
2021-11-07 13:14:27 -0500

    read...

--- read/main.go
+++ read/main.go
@@ -1,14 +1,48 @@
 package blogposts
 
 import (
-	"testing/fstest"
+	"io/fs"
+	"io"
 )
 
 type Post struct {
-	Title, Description, Body string
-	Tags []string
+	Title string
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	return nil
+func NewPostsFromFS(fileSystem fs.FS) ([]Post , error){
+	dir, err := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- read/main_test.go
+++ read/main_test.go
@@ -1,20 +1,34 @@
 package blogposts_test
 
 import (
-	blogposts "github.com/achristie/go-with-tests/read"
+	"read"
 	"testing"
 	"testing/fstest"
+	"reflect"
 )
 
 func TestNewBlogPost(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello_world.md": {Data: []byte("hi")},
-		"hello_world2.md":{Data: []byte("hola")},
+		"hello_world.md": {Data: []byte("Title: Post 1")},
+		"hello_world2.md":{Data: []byte("Title: Post 2")},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	assertPost(t, posts[0], blogposts.Post{Title: "Post 1"})
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
